3a49b21a9a7afe8ea7f8c32c6af1e4d278f5f751
brandonamezcua <brandonamezcua@008-PLN2ECS>
2020-02-03 14:06:44 -0800

    Fixed minor bugs for ex 1.13

--- Main.c
+++ Main.c
@@ -6,40 +6,40 @@
 
 int main()
 {
-    int c = EOF;
-    int i = 0;
-    int j = 0;
-    int arr_len[BIGGEST + 1];
-    int state = IN;
-    int nc = 0;
+	int c = EOF;
+	int i = 0;
+	int j = 0;
+	int arr_len[BIGGEST + 1];
+	int state = IN;
+	int nc = 0;
 
-    for (i = 0; i <= BIGGEST; ++i) {
-        arr_len[i] = 0;
-    }
-    while ((c = getchar()) != EOF)
-    {
-        ++nc;
-        if (c == ' ' || c == '\t' || c == '\n')
-        {
-            state = OUT;
-            --nc;
-        }
-        if (state == OUT)
-        {
-            if (nc != 0 && nc <= BIGGEST) {
-                ++arr_of_len[nc];
-            }
-            state = IN;
-            nc = 0;
-        }
-    }
-    for (i = 1; i <= BIGGEST; ++i)
-    {
-        printf("|%2d| ", i);
-        for (j = 0; j < arr_of_len[i]; ++j) {
-            putchar('*');
-        }
-        putchar('\n');
-    }
-    return 0;
+	for (i = 0; i <= BIGGEST; ++i) {
+		arr_len[i] = 0;
+	}
+	while ((c = getchar()) != EOF)
+	{
+		++nc;
+		if (c == ' ' || c == '\t' || c == '\n')
+		{
+			state = OUT;
+			--nc;
+		}
+		if (state == OUT)
+		{
+			if (nc != 0 && nc <= BIGGEST) {
+				++arr_len[nc];
+			}
+			state = IN;
+			nc = 0;
+		}
+	}
+	for (i = 1; i <= BIGGEST; ++i)
+	{
+		printf("|%2d| ", i);
+		for (j = 0; j < arr_len[i]; ++j) {
+			putchar('*');
+		}
+		putchar('\n');
+	}
+	return 0;
 }
